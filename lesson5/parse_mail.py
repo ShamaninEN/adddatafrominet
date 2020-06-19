ede74a92b778a4ffe230df5fde76d2f9d18d7fb7
Shamanin <shamaninen@gmail.com>
2020-06-19 18:04:09 +0500

    листаем вниз

--- lesson5/parse_mail.py
+++ lesson5/parse_mail.py
@@ -23,11 +23,11 @@ driver = webdriver.Chrome('/Users/evgeniyshamanin/gb_pars/chromedriver', options
 # driver.manage().window().maximize()
 driver.get('https://mail.ru')
 elem = driver.find_element_by_id('mailbox:login')
-# elem.send_keys('region-ctroy')
+
 elem.send_keys('study.ai_172')
 server_name = driver.find_element_by_id('mailbox:domain')
 select = Select(server_name)
-# select.select_by_visible_text('@bk.ru')
+
 select.select_by_visible_text('@mail.ru')
 
 
@@ -37,7 +37,7 @@ elem = driver.find_element_by_id('mailbox:password')
 elem = WebDriverWait(driver, 5).until(
     EC.element_to_be_clickable((By.ID,'mailbox:password'))
 )
-# elem.send_keys('89000962124regionCtroy')
+
 elem.send_keys('NextPassword172')
 
 server_name = driver.find_element_by_id('mailbox:submit')
@@ -50,14 +50,21 @@ time.sleep(10)
 # driver.send_keys(Keys.END)
 # time.sleep(15)
 links = driver.find_elements_by_xpath('//a[contains(@href, "/inbox/0")]')
+# for link in links:
+#     print(link.get_attribute('href'))
 for link in links:
-    print(link.get_attribute('href'))
-for i in range(5):
-    time.sleep(5)
-    articles = WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located(links))
+    letter = {}
+    try:
+        letter['link'] = link.get_attribute('href')
+    except:
+        print('нету линк')
+    time.sleep(2)
+    articles = links
     actions = ActionChains(driver)
     actions.move_to_element(links[-1])
     actions.perform()
+    links = driver.find_elements_by_xpath('//a[contains(@href, "/inbox/0")]')
+    print(letter)
 
 # link = WebDriverWait(driver, 5).until(
 #     EC.element_to_be_clickable((By.CLASS_NAME,'llc js-tooltip-direction_letter-bottom js-letter-list-item llc_pony-mode llc_normal'))
