9fc8c5eb130c3c341cf100580a57997d809e0a2d
Shamanin <shamaninen@gmail.com>
2020-06-23 11:55:47 +0500

    Загружает страницу ждет 5 сек потом ищет кнопку и пролистывает список хитов. После этого в теге ul появляются все теги li в которых в теге a содержится вся инфа по товару в строке, которая отлично переваривается в json и складываем в базу

--- lesson5/parse_mvideo.py
+++ lesson5/parse_mvideo.py
@@ -1,25 +1,36 @@
+import json
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select, WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+from pymongo import MongoClient
 chrome_options = Options()
 chrome_options.add_argument('start-fullscreen')
-
+client = MongoClient('mongodb://localhost:27017/')
+db = client['geeekbrains']
+collection_mvideo = db['mvideo']
 driver = webdriver.Chrome('/Users/evgeniyshamanin/gb_pars/chromedriver', options=chrome_options)
 
 driver.get('https://mvideo.ru')
-# time.sleep(5)
-# a = driver.find_element_by_class_name('btn btn-approve-city')
 
-button = WebDriverWait(driver, 5).until(
-    EC.element_to_be_clickable(driver.find_element_by_class_name('btn btn-approve-city'))
-)
+time.sleep(5)
+button = driver.find_element_by_xpath("//div[contains(text(),'Хиты')]/parent::node()/parent::node()/following-sibling::node() //a[@class='next-btn sel-hits-button-next']")
+actions = ActionChains(driver)
+actions.move_to_element(button)
 button.click()
+time.sleep(1)
+button.click()
+time.sleep(1)
+button.click()
+time.sleep(1)
+button.click()
+actions.perform()
 
-# list = driver.find_elements_by_class_name('gallery-list-item height-ready')
-# for elem in list:
-#     print(elem.get_attribute())
+items = driver.find_elements_by_xpath("//div[contains(text(),'Хиты')]/parent::node()/parent::node()/following-sibling::node() //li[@class='gallery-list-item height-ready']")
+for item in items:
+    name = item.find_element_by_xpath(".//h4/a").get_attribute("data-product-info")
+
+    b = json.loads(name)
+    collection_mvideo.insert_one(b)
+    print(b)
