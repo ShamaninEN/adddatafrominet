5784897a9dad4d785721f2a57fe158c515c817a7
Shamanin <shamaninen@gmail.com>
2020-06-24 12:05:20 +0500

    Заходит в почтовый ящик ждет загрузки письма, парсит его и складывает в базу, потом переходит к следующему письму и делает то же самое

--- lesson5/parse_mail.py
+++ lesson5/parse_mail.py
@@ -1,68 +1,77 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
+
 import time
 chrome_options = Options()
 chrome_options.add_argument('start-fullscreen')
 
-# 1) Написать программу, которая собирает входящие письма из своего или тестового почтового ящика и сложить данные о письмах в базу данных
-# * от кого,
-# * дата отправки,
-# * тема письма,
-# * текст письма полный
-# Логин тестового ящика: study.ai_172@mail.ru
-# Пароль тестового ящика: NextPassword172
-# 3) Написать программу, которая собирает «Хиты продаж» с сайта техники mvideo и складывает данные в БД.
-# Магазины можно выбрать свои. Главный критерий выбора: динамически загружаемые товары
+from pymongo import MongoClient
+chrome_options = Options()
+chrome_options.add_argument('start-fullscreen')
+client = MongoClient('mongodb://localhost:27017/')
+db = client['geeekbrains']
+collection_mail = db['mail']
 
 driver = webdriver.Chrome('/Users/evgeniyshamanin/gb_pars/chromedriver', options=chrome_options)
-# driver.manage().window().maximize()
+
 driver.get('https://mail.ru')
+
 elem = driver.find_element_by_id('mailbox:login')
 
 elem.send_keys('study.ai_172')
 server_name = driver.find_element_by_id('mailbox:domain')
 select = Select(server_name)
-
+#
 select.select_by_visible_text('@mail.ru')
-
-
+#
+#
 server_name.submit()
-
+#
 elem = driver.find_element_by_id('mailbox:password')
 elem = WebDriverWait(driver, 5).until(
     EC.element_to_be_clickable((By.ID,'mailbox:password'))
 )
-
+#
 elem.send_keys('NextPassword172')
 
 server_name = driver.find_element_by_id('mailbox:submit')
 
-# зависает в загрузке пришлось перезагружать окно
 server_name.submit()
-time.sleep(2)
-driver.refresh()
+
+time.sleep(5)
+letter_link = driver.find_element_by_xpath("//div[@class='dataset-letters'] //a[contains(@class, 'js-tooltip-direction_letter-bottom')]")
+letter_link.click()
 time.sleep(5)
 
-def show_links():
-    links = driver.find_elements_by_xpath('//a[contains(@href, "/inbox/0")]')
 
-    last_letter = links[-1]
-    print(last_letter)
-    for link in links:
-        print(link.get_attribute('href'))
-    return last_letter
+def letter_parse(driver):
+    letter = {}
+    letter['theme'] = driver.find_element_by_xpath("//h2[@class='thread__subject thread__subject_pony-mode']").text
+    letter['author_mail'] = driver.find_element_by_xpath(
+        "//div[@class='letter__author']/span[@class='letter-contact']").get_attribute('title')
+    letter['author_name'] = driver.find_element_by_xpath(
+        "//div[@class='letter__author']/span[@class='letter-contact']").get_attribute('title')
+    letter['letter_time'] = driver.find_element_by_xpath("//div[@class='letter__date']").text
+    letter['letter_body'] = driver.find_element_by_xpath("//div[@class='letter__body']").text
+    collection_mail.insert_one(letter)
+    return letter
 
-show_links()
-i = 0
-while True:
-    i +=1
-    articles = driver.find_elements_by_xpath('//a[contains(@href, "/inbox/0")]')
-    actions = ActionChains(driver)
-    actions.move_to_element(show_links())
-    time.sleep(1)
-    actions.perform()
+
+print(letter_parse(driver))
+
+i = True
+
+while i:
+    try:
+        time.sleep(3)
+        button = driver.find_element_by_xpath("//div[@class='portal-menu-element portal-menu-element_next portal-menu-element_expanded portal-menu-element_not-touch portal-menu-element_pony-mode']")
+    except:
+        i = False
+    else:
+        button.click()
+        time.sleep(5)
+        print(letter_parse(driver))
